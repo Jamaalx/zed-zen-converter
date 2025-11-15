6e3a380b3ada1dff3c86e354ab2ab086d33b8518
Jamaalx <alex.mantello13@gmail.com>
2025-11-15 09:50:30 +0200

    dada

--- .vite/build/preload.js
+++
@@ -1,1 +0,0 @@
-"use strict";const{contextBridge:i,ipcRenderer:o}=require("electron");i.exposeInMainWorld("electronAPI",{selectFiles:()=>o.invoke("select-files"),selectFolder:()=>o.invoke("select-folder"),getFileInfo:e=>o.invoke("get-file-info",e),convertFile:e=>o.invoke("convert-file",e),openFolder:e=>o.invoke("open-folder",e)});
